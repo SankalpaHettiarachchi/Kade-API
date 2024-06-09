78519dc4b3cf5bc0a92f4494f6894ee86fcaa9f0
Sankalpa Hettiarachchi <113717231+SankalpaHettiarachchi@users.noreply.github.com>
2024-06-09 18:51:13 +0530

    Product updater done

--- Kade-API/app/Http/Controllers/Api/ProductController.php
+++ Kade-API/app/Http/Controllers/Api/ProductController.php
@@ -22,46 +22,39 @@ class ProductController extends Controller
     public function store(ProductCreateRequest $request)
     {
         $request->Validated($request->all());
-        DB::table('products')->insert([
-            'user_id' => Auth::id(),
-            'image_url' => $request->image_url,
-            'name' => $request->name,
-            'description' => $request->description,
-            'av_quantity' => $request->quantity,
-            'quantity' => $request->quantity,
-            'unit' => $request->unit,
-            'unit_price' => $request->unit_price,
-        ]);
-    }
+        $newProduct = DB::table('products')->insertGetId([
+                'user_id' => Auth::id(),
+                'image_url' => $request->image_url,
+                'name' => $request->name,
+                'description' => $request->description,
+                'av_quantity' => $request->quantity,
+                'quantity' => $request->quantity,
+                'unit' => $request->unit,
+                'unit_price' => $request->unit_price,
+            ]);
 
+        $product = Product::findOrFail($newProduct);
+        return new ProductCollection(collect([$product]));
+
+    }
 
     public function show(Request $request, Product $product)
     {
         //
-
     }
 
-
-    public function update(Request $request, Product $product)
+    public function update(ProductUpdateRequest $request, Product $product)
     {
-        // $request->Validated($request->all());
+        $request->Validated($request->all());
         $product->update($request->only([
             'image_url','name','description','av_quantity','unit','unit_price'
         ]));
 
-        // DB::table('products')->insert([
-        //     'id' => $request->id,
-        //     'image_url' => $request->image_url,
-        //     'name' => $request->name,
-        //     'description' => $request->description,
-        //     'av_quantity' => $request->quantity,
-        //     'unit' => $request->unit,
-        //     'unit_price' => $request->unit_price,
-        // ]);
+        $product = Product::findOrFail($product->id);
+        return new ProductCollection(collect([$product]));
     }
 
-
-    public function destroy(Product $product)
+    public function destroy(Product $request)
     {
         //
     }
